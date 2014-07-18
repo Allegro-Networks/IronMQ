58896bbff50b3d63157c846a0aa1772ccd905431
ruby_gem <gemma.cameron@gmail.com>
2014-07-18 16:56:14 +0100

    checking the payload

--- MessageQueueEndpoint.rb
+++ MessageQueueEndpoint.rb
@@ -8,8 +8,9 @@ QUEUE_NAME = 'friday_test_for_reals'
 
 post '/bob' do 
 	message_queue = MessageQueue.new(QUEUE_NAME)
-	
-	puts ">>>>>>>>>>>>>>>>>>>>>>#{message_queue.pop}"
+	puts "\n ON QUEUE >>>>>>>>>>>>>>>>>>>>>> #{message_queue.pop}\n"
+
+	puts "\n ON PAYLOAD <<<<<<<<<<<<<<<<<<<< #{data = JSON.parse(request.body.read)}"
 end
 
 get '/' do
--- test/test_push_to_subscribed_queue.rb
+++ test/test_push_to_subscribed_queue.rb
@@ -8,12 +8,12 @@ class TestPop < Test::Unit::TestCase
 		assert_not_nil(ENV['IRON_MQ_HOST'])
 	end
 	def test_pop
-		# queue_name = "friday_test_for_reals"
+		queue_name = "friday_test_for_reals"
 		 words = "Gemma has done it mofo #{DateTime.now}"
 
-		# message_queue = SubscribedMessageQueue.new(queue_name)
+		message_queue = SubscribedMessageQueue.new(queue_name)
 
-		# #message_queue.push(words)
+		message_queue.push(words)
 		puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #{words}"
 	end
 end
